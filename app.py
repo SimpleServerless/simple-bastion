22512f0f61518a30a3bd61bb520ee0b798cbb0bd
ebarnes <ed@intelligems.io>
2025-08-24 23:19:30 -0600

    updated 2025

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # Shamelessly lifted from https://medium.com/faun/create-a-bastion-with-aws-cdk-d5ebfb91aef9
 
+
 from aws_cdk import (
-    core
+    Stack,
+    CfnOutput,
+    App,
+    aws_ec2 as ec2,
 )
-from aws_cdk.aws_ec2 import BastionHostLinux, InstanceType, AmazonLinuxImage, \
-    SubnetSelection, SecurityGroup, SubnetType, Vpc
+from constructs import Construct
 import os
 import boto3
 import logging
@@ -14,30 +17,30 @@ log = logging.getLogger()
 ssm = boto3.client('ssm')
 vpc_id: str = ssm.get_parameter(Name="VpcId")['Parameter']['Value']
 
-class CdkStack(core.Stack):
-    security_group: SecurityGroup
+class CdkStack(Stack):
+    security_group: ec2.SecurityGroup
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        vpc = Vpc.from_lookup(self, 'VPC', vpc_id=vpc_id)
+        vpc = ec2.Vpc.from_lookup(self, 'VPC', vpc_id=vpc_id)
         self.security_group = self.__create_security_group(vpc)
 
-        bastion = BastionHostLinux(
+        bastion = ec2.BastionHostLinux(
             self, id,
             vpc=vpc,
             instance_name='simple-bastion',
-            instance_type=InstanceType('t3a.nano'),
-            machine_image=AmazonLinuxImage(),
-            subnet_selection=SubnetSelection(subnet_type=SubnetType.PUBLIC),
+            instance_type=ec2.InstanceType('t3a.nano'),
+            machine_image=ec2.AmazonLinuxImage(),
+            subnet_selection=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
             security_group=self.security_group,
         )
-        core.CfnOutput(self, 'bastion-id-output', value=bastion.instance_id, export_name="BastionInstanceId")
-        core.CfnOutput(self, "bastion-sg-output", value=self.security_group.security_group_id, export_name="BastionSG")
+        CfnOutput(self, 'bastion-id-output', value=bastion.instance_id, export_name="BastionInstanceId")
+        CfnOutput(self, "bastion-sg-output", value=self.security_group.security_group_id, export_name="BastionSG")
 
-    def __create_security_group(self, vpc: Vpc) -> SecurityGroup:
-        return SecurityGroup(
-            scope=self,
+    def __create_security_group(self, vpc: ec2.Vpc) -> ec2.SecurityGroup:
+        return ec2.SecurityGroup(
+            self,
             id='bastion-sg',
             security_group_name='bastion-sg',
             description='Security group for the bastion, no inbound open because we should access'
@@ -46,7 +49,7 @@ class CdkStack(core.Stack):
             allow_all_outbound=True
         )
 
-app = core.App()
+app = App()
 
 account = os.environ['AWS_ACCOUNT']
 region = os.environ['AWS_DEFAULT_REGION']
